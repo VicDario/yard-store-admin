5b2808d2dc1c4d992689190857559acfe752bb75
VicDario <26585356+VicDario@users.noreply.github.com>
2022-07-08 09:18:16 -0400

    [Add] Service addProduct

---
+++ src/services/api/product.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import endPoints from '@services/api';
+
+const addProduct = async body => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+      accept: '*/*'
+    }
+  };
+  const response = await axios.post(endPoints.products.addProducts, body, config);
+  return response.data;
+};
+
+export { addProduct };
